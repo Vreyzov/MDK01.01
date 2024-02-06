79e6a636f52f315659169ff555225e6f4620d07f
Vreyzov <115491934+Vreyzov@users.noreply.github.com>
2024-02-06 12:00:16 +0300

    Add files via upload

---
+++ Var_6/Var_6/Classes/Clsprice.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var_6.Classes
+{
+    
+        internal class Price
+        {
+            //Поля
+            private string productName;
+            private string storeName;
+            private double productPrice;
+            private int productQuantity;
+
+            public string ProductName
+            {
+                get { return productName; }
+                set { productName = value; }
+            }
+
+            public string StoreName
+            {
+                get { return storeName; }
+                set { storeName = value; }
+            }
+
+            public double ProductPrice
+            {
+                get { return productPrice; }
+                set { productPrice = value; }
+            }
+
+            public int ProductQuantity
+            {
+                get { return productQuantity; }
+                set { productQuantity = value; }
+            }
+
+            //Конструктор по умолчанию
+            public Price()
+            {
+            }
+
+            //Конструктор с параметрами
+            public Price(string productName, string storeName, double productPrice, int productQuantity)
+            {
+                this.productName = productName;
+                this.storeName = storeName;
+                this.productPrice = productPrice;
+                this.productQuantity = productQuantity;
+            }
+
+            //Метод ввода данных объекта
+            public void VvodDannih()
+            {
+                Console.WriteLine("Введите название товара:");
+                productName = Console.ReadLine();
+
+                Console.WriteLine("Введите название магазина:");
+                storeName = Console.ReadLine();
+
+                Console.WriteLine("Введите стоимость товара:");
+                double.TryParse(Console.ReadLine(), out productPrice);
+
+                Console.WriteLine("Введите количество товара:");
+                int.TryParse(Console.ReadLine(), out productQuantity);
+            }
+
+            //Метод вывода данных объекта
+            public override string ToString()
+            {
+                return $"*********************\n" + $"Товар: {productName}\n" + $"Магазин: {storeName}\n" + $"Стоимость: {productPrice} руб.\n" + $"Количество: {productQuantity}";
+            }
+        //Метод расчета общей стоимости товаров в магазине
+        public static double CalculateTotalPriceInStore(Price[] prices, string storeName)
+        {
+            double total = 0;
+
+            foreach (Price price in prices)
+            {
+                if (price.StoreName.Equals(storeName, StringComparison.OrdinalIgnoreCase))
+                {
+                    total += price.ProductPrice * price.ProductQuantity;
+                }
+            }
+
+            return total;
+        }
+            //Метод поиска информации о товаре по названию
+            public static Price FindProductByName(Price[] prices, string productName)
+            {
+                foreach (Price price in prices)
+                {
+                    if (price.ProductName.Equals(productName, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return price;
+                    }
+                }
+
+                return null;
+            }
+        }
+    
+}
---
+++ Var_6/Var_6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using Var_6.Classes;
+
+    
+
+        Console.WriteLine("Введите количество товаров:");
+        int itemCount;
+        while (!int.TryParse(Console.ReadLine(), out itemCount) || itemCount <= 0)
+        {
+            Console.WriteLine("Введите корректное количество товаров (целое положительное число):");
+        }
+
+    Price[] prices = new Price[itemCount];
+
+        //Ввод данных о товарах
+        for (int i = 0; i < itemCount; i++)
+        {
+            prices[i] = new Price();
+            prices[i].VvodDannih();
+        }
+
+        //Вывод данных о товарах
+        Console.WriteLine("\nИнформация о товарах:");
+        foreach (Price price in prices)
+        {
+            Console.WriteLine(price.ToString());
+        }
+         //Метод расчета общей стоимости товаров в магазине
+        Console.WriteLine("\nРасчет общей стоимости товаров в магазине:");
+        Console.Write("Введите название магазина для расчета: ");
+        string storeToCalculate = Console.ReadLine();
+
+        double totalInStore = Price.CalculateTotalPriceInStore(prices, storeToCalculate);
+        if (totalInStore > 0)
+        {
+            Console.WriteLine($"Общая стоимость товаров в магазине {storeToCalculate}: {totalInStore} руб.");
+        }
+        else
+        {
+            Console.WriteLine($"Магазин {storeToCalculate} не найден или в нем нет товаров.");
+        }
+
+
+        //Поиск информации о товаре по названию
+        Console.WriteLine("\nПоиск информации о товаре по названию:");
+        Console.Write("Введите название товара для поиска: ");
+        string productToFind = Console.ReadLine();
+
+    Price foundProduct = Price.FindProductByName(prices, productToFind);
+        if (foundProduct != null)
+        {
+            Console.WriteLine($"Информация о товаре:\n{foundProduct.ToString()}");
+        }
+        else
+        {
+            Console.WriteLine($"Товар с названием {productToFind} не найден.");
+        }
+
